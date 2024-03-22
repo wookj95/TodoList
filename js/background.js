347956b3b12be3754034a079235d311923e0a71a
wookj95 <wookj94@naver.com>
2024-03-22 16:58:22 +0900

    commit

---
+++ js/background.js
@@ -0,0 +1,17 @@
+const images =[
+    "img01.jpg",
+    "img02.jpg",
+    "img03.jpg",
+    "img04.jpg",
+    "img05.jpg"
+];
+const BG = "background"
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.className = BG;
+
+bgImage.src = `img/${chosenImage} `;
+
+document.body.appendChild(bgImage);
