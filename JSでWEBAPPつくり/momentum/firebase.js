b28c5120a217eca4bde19067e2cdb04de77c063b
wookj95 <87466448+wookj95@users.noreply.github.com>
2024-05-15 15:13:20 +0900

    Add files via upload

---
+++ JSでWEBAPPつくり/momentum/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMIj-7_8JzLjUCDHOBa-H_R9AQvc8tqS4",
+  authDomain: "js-todolist-3d643.firebaseapp.com",
+  projectId: "js-todolist-3d643",
+  storageBucket: "js-todolist-3d643.appspot.com",
+  messagingSenderId: "705618940798",
+  appId: "1:705618940798:web:a338f42a053416d0a0192f",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
